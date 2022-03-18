0018eb960725e94fc71a0618dc52a6274a9952ee
Gautamsharma02 <gautamji94624@gmail.com>
2022-03-18 10:39:36 +0530

    first commit

---
+++ js/furniture.js
@@ -0,0 +1,3 @@
+$(".SearchIcon").click(function() {
+    $('#Headersearch').addClass("activesearch");
+});
---
+++ js/new.js
@@ -0,0 +1,9 @@
+let white = document.getElementById('whiteDivLine');
+
+function whiteDiv() {
+    white.classList.add('activeWhite');
+};
+
+$(".SearchIcon").on('click', function() {
+    $('#Headersearch').addClass("activesearch");
+});
